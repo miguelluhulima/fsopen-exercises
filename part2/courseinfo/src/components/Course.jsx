6f346441492e911f9753e5fb99eb8cb6a103b147
miguelluhulima <miguelluhu5@gmail.com>
2025-07-16 10:32:26 +0700

    2.4 submission, courseinfo

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -3,13 +3,19 @@ import Content from './Content'
 import Total from './Total'
 
 const Course = (props) => {
-    return (
-        <div>
-            <Header text={props.course.name} />
-            <Content parts={props.course.parts} />
-            <Total parts={props.course.parts} />
-        </div>
-    )
+  return (
+    <>
+      {
+        props.courses.map(course =>
+          <div key={course.id}>
+            <Header text={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
+          </div>
+        )
+      }  
+    </>
+  )
 }
 
 export default Course
