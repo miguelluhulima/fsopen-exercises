31d9c5bc12e601c12f713a49437f969502e084d2
miguelluhulima <miguelluhu5@gmail.com>
2025-08-05 17:01:45 +0700

    3.4 submission, phonebook-backend

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,26 +1,26 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+let persons = [
   {
-    "id": "1",
-    "name": "Arto Hellas",
-    "number": "040-123456"
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456"
   },
   {
-    "id": "2",
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523"
   },
   {
-    "id": "3",
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345"
   },
   {
-    "id": "4",
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122"
   }
 ]
 
@@ -51,6 +51,14 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(p => p.id !== id)
+  console.log(persons)
+
+  response.status(204).end()
 })
 
 const PORT = 3001
